215cccbc5910b489adfb31b4124f9ea910a0e778
Crowley-Coronel <sk_8_917@hotmail.com>
2020-12-24 10:48:38 -0600

    Mejorando codigo y agregando tema dark

--- src/components/avatar-select/avatar-select.tsx
+++ src/components/avatar-select/avatar-select.tsx
@@ -1,5 +1,4 @@
 import { Component, Prop, h, Event, EventEmitter } from '@stencil/core';
-// import { format } from '../../utils/utils';
 import { avatars } from './avatar.js';
 
 @Component({
@@ -13,36 +12,36 @@ export class AvatarSelect {
   @Prop() avatar: string;
   @Event() avatarSelected: EventEmitter;
   @Prop() opciones: string;
-  
-  constructor(){
-   
-    let avatar_selected = { index: '', avatar: ''};
-        
-    if(window.localStorage.getItem('avatar') != null ){
+
+  constructor() {
+    let avatar_selected = { index: '', avatar: '' };
+    if (window.localStorage.getItem('avatar') != null) {
       avatar_selected = JSON.parse(window.localStorage.getItem('avatar'));
       this.avatar = avatar_selected.avatar;
-    }else{
-      this.avatar =  avatars[0].imagen;
-    }  
-   
+    } else {
+      this.avatar = avatars[0].imagen;
+    }
+
   }
 
- 
-   public getAvatars(){
-      let array_r = [];
-      avatars.forEach((element,index) => {
-         array_r.push(
-                      <div class="item"> 
-                        <img src={ element.imagen } 
-                             onClick={ () => this.set_avatar(index) }
-                             class="avatar_flex_grid_element" /> 
-                      </div> 
-                      )
-      });
-      return array_r;
+  public getAvatars() {
+    let array_r = [];
+    let item_class = this.opciones.split(' ');
+    let itm =  item_class[1] == undefined ? 'item' : 'item-' + item_class[1];
+
+    avatars.forEach((element, index) => {
+      array_r.push(
+        <div class={ itm }>
+          <img src={element.imagen}
+            onClick={() => this.set_avatar(index)}
+            class="avatar_flex_grid_element" />
+        </div>
+      )
+    });
+    return array_r;
   }
 
-  public set_avatar(index = 0){
+  public set_avatar(index = 0) {
     this.avatar = avatars[index].imagen;
     this.drop = false;
     let avatar_seleccionado = {
@@ -53,33 +52,29 @@ export class AvatarSelect {
     window.localStorage.setItem('avatar', JSON.stringify(avatar_seleccionado));
   }
 
-  public activar (){
+  public activar() {
     this.drop = !this.drop;
     return this.drop;
   }
 
   render() {
-
-    if(this.drop){
-      return    <div class="popover__wrapper">
-                      <img src={this.avatar }
-                           onClick={() => this.activar() } 
-                           class="static_avatar"
-                      />
-                    <div class={ 'popover__content ' + this.opciones } >
-                      <div class="container">
-                        { this.getAvatars() }
-                      </div>
-                  </div>
-                </div>
-    }else{
-      return <div class="popover__wrapper">
-                <img src={ this.avatar }  
-                     onClick={() => this.activar() } 
-                     class="static_avatar"
-                />
-              </div>
-    }
-  
+      return (
+      <div class="popover__wrapper">
+        <img src={this.avatar}
+          onClick={() => this.activar()}
+          class="static_avatar"
+        />
+        { 
+        //Short Circuit Evaluation with &&
+        //https://www.digitalocean.com/community/tutorials/7-ways-to-implement-conditional-rendering-in-react-applications
+        this.drop &&
+          <div class={'popover__content ' + this.opciones}>
+            <div class="container">
+              {this.getAvatars()}
+            </div>
+          </div>
+        }
+      </div>
+      )
   }
 }
